23daca89586d7c4c0b6a732cc126c9f47b0243d1
Jake Caza <jake.caza@gmail.com>
2025-10-09 22:10:22 -0400

    Make more readable

--- get_monitor_numbers.ps1
+++ get_monitor_numbers.ps1
@@ -1,1 +1,18 @@
-$MonitorNames = Get-WmiObject -Namespace root\wmi -ClassName WmiMonitorID | ForEach-Object { if ($_.UserFriendlyName) { [System.Text.Encoding]::ASCII.GetString($_.UserFriendlyName -ne 0) } else { "Generic Monitor" } }; Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Screen]::AllScreens | ForEach-Object -Begin {$i=1} -Process { "$i`: $($MonitorNames[$i-1]) ($($_.DeviceName))"; $i++ }
+# Get monitor names from WMI
+$MonitorNames = Get-WmiObject -Namespace root\wmi -ClassName WmiMonitorID | ForEach-Object {
+    if ($_.UserFriendlyName) {
+        [System.Text.Encoding]::ASCII.GetString($_.UserFriendlyName -ne 0)
+    } else {
+        "Generic Monitor"
+    }
+}
+
+# Load Windows Forms assembly to access screen information
+Add-Type -AssemblyName System.Windows.Forms
+
+# Display each monitor with its number, name, and device name
+$i = 1
+[System.Windows.Forms.Screen]::AllScreens | ForEach-Object {
+    "$i`: $($MonitorNames[$i-1]) ($($_.DeviceName))"
+    $i++
+}
